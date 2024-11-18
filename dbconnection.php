4ad729d503c3a2aab5619209a0859bae0cc3a5cc
aloien/KenSolisa <159671468+Aloien@users.noreply.github.com>
2024-11-18 17:33:21 +0800

    updated dbconnection.php

--- dbconnection.php
+++ dbconnection.php
@@ -1,12 +1,22 @@
 <?php
-$servername = "localhost";  
-$username = "root";        
-$password = "";            
-$dbname = "reglog";        
+class Database {
+    private $host = "localhost";
+    private $db_name = "reglog";
+    private $username = "root";
+    private $password = "";
+    public $conn;
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+    public function getConnect() {
+        $this->conn = null;
 
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+        try {
+            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch(PDOException $exception) {
+            echo "Connection error: " . $exception->getMessage();
+        }
+
+        return $this->conn;
+    }
 }
 ?>
