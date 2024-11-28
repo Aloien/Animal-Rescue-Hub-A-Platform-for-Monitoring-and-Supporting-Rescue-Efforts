83bb2286e6c3b450f690c8efbbce3105ef8e7b71
ZerepAldren <adlrenperez04@gmail.com>
2024-11-28 16:04:33 +0800

    change db directory

--- classes/dbconnection.php
+++ classes/dbconnection.php
@@ -1,7 +1,7 @@
 <?php
 class Database {
     private $host = "localhost";
-    private $db_name = "reglog";
+    private $db_name = "incident_reports";
     private $username = "root";
     private $password = "";
     public $conn;
